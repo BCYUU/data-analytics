a7b63d801e50188f664f763e6b89b60c8e8806dc
BCYUU <bcollins@my.yearupunited.org>
2026-04-13 14:31:52 -0400

    Exercise 1.C complete

---
+++ week-02/Ex1C_sakila_exploration.sql
@@ -0,0 +1,11 @@
+/*
+a) Info in the actor tab includes Actor Id, first and last name, and last update
+b) Ok thats a lot of categories so here are 4 that exemplify it, title , rental duration, length, rating
+c)  The answer is Film_Actor
+d) There are 1000 rows so it is not easy to look at all the data
+e) Again 1000 rows so not easy to go through
+f) I would use rental and film, both have parts of what I need for the question
+*/
+
+select * from film;
+select * from rental;
