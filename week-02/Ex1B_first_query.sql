1d6bddf2f76cb857ad9a7e4e05f9d4102d9f1920
BCYUU <bcollins@my.yearupunited.org>
2026-04-13 14:04:50 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 A list of actors and movies etc produced in xyz for comparing box office success
 */
+
+USE sakila;
+Select * From actor; -- Retrieved 200 records
+
+select * from film -- Retrieved 1000 records
