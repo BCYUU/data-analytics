5e9d21987d8c466a71b6d3656c54188c180a959b
BCYUU <bcollins@my.yearupunited.org>
2026-05-04 14:28:07 -0400

    done with this one

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,22 @@
 print('Hello world!')
+
+
+message = 'Hello world'
+print(message)
+
+# its printing twice since there are two lines asking for it 
+
+# Displaying dollars and cents
+
+
+dollars = 3
+cents = .50
+print(dollars+cents)
+
+# result adds the integer to the float  presumably by converting the integer to a float.
+cents = cents +.25
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(d_str + " " + c_str)
