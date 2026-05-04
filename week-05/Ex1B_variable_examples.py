9b387cb99bede8cfbfa4fe7209c2374dffb8174c
BCYUU <bcollins@my.yearupunited.org>
2026-05-04 14:56:10 -0400

    aha looks good man sure

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,34 @@
+#going to use camel case
+cusID = 11111 #int
+cusGen = blue #string
+cusName = Joe #string
+cusDOB = "01/02/2026" #I guess string -edit: yeah had to make it a string
+driverLN = 1238971239 # int
+autoPN = 198743672 # int 
+# most of these numbers could probably be stored as strings if needed since they shouldnt change
+
+name = Blake
+city = location
+state = NC 
+
+# a) What is the full list of reserved words that can’t be used for variable names?
+
+# False	def	if	raise
+# None	del	import	return
+# True	elif	in	try
+# and	else	is	while
+# as	except	lambda	with
+# assert	finally	nonlocal	yield
+# break	for	not
+# class	form	or
+# continue	global	pass
+
+# b) Pick 5 of these words and review the explanation for how it is used as a keyword in
+# Python. Add these 5 definitions as # comments to your exercise document. Put ^^
+# around any terms that you are not familiar with.
+
+# false - boolean not true
+# none - represents the absence of a value
+# true - boolean true
+# and - logi operator that equates for both true
+# as - reasigns a new name/ identifier
